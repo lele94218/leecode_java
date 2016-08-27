c2f426c9f6f9d8a34d5f8ad3458f40fa02d43802
lele94218 <terry.trxue@gmail.com>
2016-08-26 20:41:46 -0400

    haha

--- src/com/terryx/main/Main.java
+++ src/com/terryx/main/Main.java
@@ -6,6 +6,7 @@ import com.terryx.leecode.Solution1;
  * @author xueta on 8/25/2016 8:06 AM.
  */
 public class Main {
+    //haha
     public static void main(String[] args) {
         Solution1 solution1 = new Solution1();
         solution1.twoSum(new int[]{0, 4, 3, 0}, 0);
