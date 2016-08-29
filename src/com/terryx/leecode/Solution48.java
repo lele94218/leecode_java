dbe060d2f4e1b52bef8b0066fa43caf226c72913
lele94218 <terry.trxue@gmail.com>
2016-08-29 15:49:29 -0400

    48

---
+++ src/com/terryx/leecode/Solution48.java
@@ -0,0 +1,29 @@
+package com.terryx.leecode;
+
+/**
+ * @author taoranxue on 8/29/16 3:22 PM.
+ */
+public class Solution48 {
+    public void rotate(int[][] matrix) {
+        int n = matrix[0].length;
+        // up is down
+        for (int i = 0; i < n / 2; ++i) {
+            for (int j = 0; j < n; ++j) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[n - i - 1][j];
+                matrix[n - i - 1][j] = tmp;
+            }
+        }
+
+        // dui jiao xian
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i; ++j)
+                if (i != j) {
+                    int tmp = matrix[i][j];
+                    matrix[i][j] = matrix[j][i];
+                    matrix[j][i] = tmp;
+                }
+        }
+    }
+}
--- src/com/terryx/main/Main.java
+++ src/com/terryx/main/Main.java
@@ -1,17 +1,20 @@
 package com.terryx.main;
 
-import com.terryx.leecode.Solution1;
-import com.terryx.leecode.Solution144;
+import com.terryx.leecode.*;
 
 /**
  * @author xueta on 8/25/2016 8:06 AM.
  */
 public class Main {
     public static void main(String[] args) {
-        Solution144 solution144 = new Solution144();
-        TreeNode root = new TreeNode(1);
-        root.right = new TreeNode(2);
-        root.right.right = new TreeNode(3);
-        solution144.preorderTraversal(root);
+        Solution48 solution48 = new Solution48();
+        int[][] a = {{1, 2}, {3, 4}};
+        solution48.rotate(a);
+        for (int i = 0; i < a[0].length; ++i) {
+            for (int j = 0; j < a[0].length; ++j) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
