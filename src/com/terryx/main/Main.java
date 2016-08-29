64d5c40b7137736f989449eb4463c0997a36c25b
lele94218 <terry.trxue@gmail.com>
2016-08-28 20:00:36 -0400

    8.28

--- src/com/terryx/main/Main.java
+++ src/com/terryx/main/Main.java
@@ -1,13 +1,17 @@
 package com.terryx.main;
 
 import com.terryx.leecode.Solution1;
+import com.terryx.leecode.Solution144;
 
 /**
  * @author xueta on 8/25/2016 8:06 AM.
  */
 public class Main {
     public static void main(String[] args) {
-        Solution1 solution1 = new Solution1();
-        solution1.twoSum(new int[]{0, 4, 3, 0}, 0);
+        Solution144 solution144 = new Solution144();
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.right.right = new TreeNode(3);
+        solution144.preorderTraversal(root);
     }
 }
---
+++ src/com/terryx/main/TreeNode.java
@@ -0,0 +1,14 @@
+package com.terryx.main;
+
+/**
+ * @author taoranxue on 8/28/16 2:47 PM.
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int x) {
+        val = x;
+    }
+}
