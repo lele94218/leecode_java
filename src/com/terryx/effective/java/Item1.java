74237c4af23eb0c9660f3958cb11d835ec6c5b74
lele94218 <terry.trxue@gmail.com>
2016-09-13 16:29:51 -0400

    add effective.java

---
+++ src/com/terryx/effective/java/Item1.java
@@ -0,0 +1,7 @@
+package com.terryx.effective.java;
+
+/**
+ * @author taoranxue on 9/13/16 4:29 PM.
+ */
+public class Item1 {
+}
--- src/com/terryx/main/Main.java
+++ src/com/terryx/main/Main.java
@@ -2,12 +2,13 @@ package com.terryx.main;
 
 import com.terryx.leecode.*;
 
+import java.util.UUID;
+
 /**
  * @author xueta on 8/25/2016 8:06 AM.
  */
 public class Main {
     public static void main(String[] args) {
-        Solution105 solution105 = new Solution105();
-        solution105.buildTree(new int[] {}, new int[] {});
+
     }
 }
