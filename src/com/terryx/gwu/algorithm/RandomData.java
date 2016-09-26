d483ffdf74f434c5d3047799a3a3153d3186086e
lele94218 <terry.trxue@gmail.com>
2016-09-26 16:26:04 -0400

    add project 2

---
+++ src/com/terryx/gwu/algorithm/RandomData.java
@@ -0,0 +1,19 @@
+package com.terryx.gwu.algorithm;
+
+import java.util.Random;
+
+/**
+ * @author taoranxue on 9/26/16 2:58 AM.
+ */
+public class RandomData {
+    public static Point2D[] randomPoint2D(int n) {
+        Point2D[] points = new Point2D[n];
+        Random random = new Random();
+        for (int i = 0; i < n; ++i) {
+            double x = random.nextDouble();
+            double y = random.nextDouble();
+            points[i] = new Point2D(x, y);
+        }
+        return points;
+    }
+}
