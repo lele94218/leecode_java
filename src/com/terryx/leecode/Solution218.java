9d834e70abb4021ca3c64ed190bf5e2d25e0e9bb
lele94218 <terry.trxue@gmail.com>
2016-09-29 15:30:20 -0400

    sort tree

---
+++ src/com/terryx/leecode/Solution218.java
@@ -0,0 +1,90 @@
+package com.terryx.leecode;
+
+import java.util.*;
+
+/**
+ * @author taoranxue on 9/27/16 3:36 PM.
+ */
+public class Solution218 {
+    public List<int[]> getSkylineByTreeMap(int[][] buildings) {
+        List<Integer[]> height = new ArrayList<>();
+        List<int[]> result = new ArrayList<>();
+        for (int [] building : buildings) {
+            height.add(new Integer[]{building[0], building[2]});
+            height.add(new Integer[]{building[1], -building[2]});
+        }
+        Collections.sort(height, new Comparator<Integer[]>() {
+            @Override
+            public int compare(Integer[] p, Integer[] q) {
+                if (!Objects.equals(p[0], q[0])) return p[0] - q[0];
+                return q[1] - p[1];
+            }
+        });
+        TreeMap<Integer, Integer> mt = new TreeMap<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer p, Integer q) {
+                return q - p;
+            }
+        });
+        mt.put(0,1);
+        int prev = 0;
+        for (Integer[] h : height) {
+            if (h[1] > 0) {
+                Integer cnt = mt.get(h[1]);
+                cnt = (cnt == null) ? 1 : cnt + 1;
+                mt.put(h[1], cnt);
+            } else {
+                Integer cnt = mt.get(-h[1]);
+                if (cnt == 1) {
+                    mt.remove(-h[1]);
+                } else {
+                    mt.put(-h[1], cnt - 1);
+                }
+            }
+            int cur = mt.firstKey();
+            if (prev != cur) {
+                result.add(new int[]{h[0], cur});
+                prev = cur;
+            }
+        }
+        return result;
+    }
+
+    public List<int[]> getSkyline(int[][] buildings) {
+        List<Integer[]> height = new ArrayList<>();
+        List<int[]> result = new ArrayList<>();
+        for (int [] building : buildings) {
+            height.add(new Integer[]{building[0], building[2]});
+            height.add(new Integer[]{building[1], -building[2]});
+        }
+        Collections.sort(height, new Comparator<Integer[]>() {
+            @Override
+            public int compare(Integer[] p, Integer[] q) {
+                if (!Objects.equals(p[0], q[0])) return p[0] - q[0];
+                return q[1] - p[1];
+            }
+        });
+        Queue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer p, Integer q) {
+                return q - p;
+            }
+        });
+        pq.offer(0);
+        int prev = 0;
+        for (Integer[] h : height) {
+            System.out.println("peek: " + pq.peek());
+            if (h[1] > 0) {
+                pq.offer(h[1]);
+            } else {
+                pq.remove(-h[1]);
+            }
+            int cur = pq.peek();
+            if (prev != cur) {
+                result.add(new int[]{h[0], cur});
+                prev = cur;
+            }
+        }
+        return result;
+    }
+}
--- src/com/terryx/main/Main.java
+++ src/com/terryx/main/Main.java
@@ -1,29 +1,53 @@
 package com.terryx.main;
 
+import com.terryx.leecode.Solution218;
 import com.terryx.leecode.Solution23;
 
-import java.util.Random;
+import java.util.*;
 
 /**
  * @author xueta on 8/25/2016 8:06 AM.
  */
 public class Main {
     public static void main(String args[]) {
-        Solution23 solution23 = new Solution23();
-        Solution23.ListNode[] listNodes = new Solution23.ListNode[10000];
-        Random random = new Random();
-        for (int i = 0; i < 10000; ++ i) {
-            listNodes[i] = new Solution23.ListNode(random.nextInt()%100);
+        Solution218 solution218 = new Solution218();
+        List<int[]> list = solution218
+                .getSkylineByTreeMap(new int[][]{new int[]{0, 10, 22}, new int[]{2, 3, 22}, new int[]{6, 7, 22}});
+        for (int[] ints : list) {
+            System.out.println(ints[0] + ", " + ints[1]);
         }
 
-        Solution23.ListNode listNode = solution23.mergeKLists(new Solution23.ListNode[]{new Solution23.ListNode(5),new Solution23.ListNode(2)});
-//        System.out.println(listNode);
-        System.out.println("ok");
-        while (listNode != null) {
-            System.out.print(listNode + " ");
-            listNode = listNode.next;
-        }
-        System.out.println();
+        TreeMap<Integer, Integer> treeMap = new TreeMap<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer p, Integer q) {
+                return q - p;
+            }
+        });
+        treeMap.put(10, 0);
+        treeMap.put(22, 0);
+        treeMap.put(22, 0);
+        treeMap.put(1, 0);
+        System.out.println(treeMap.firstKey());
+        treeMap.remove(22);
+        System.out.println(treeMap.firstKey());
+
+        Queue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer p, Integer q) {
+                return q - p;
+            }
+        });
+        System.out.println("pq:");
+        pq.offer(10);
+        pq.offer(22);
+        pq.offer(22);
+        pq.offer(1);
+        System.out.println(pq.peek());
+        pq.remove(22);
+        System.out.println(pq.peek());
+        pq.remove(22);
+        System.out.println(pq.peek());
+
     }
 
 }
