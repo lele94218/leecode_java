535052aaf4622d687e580b98f7078a9c348c0416
lele94218 <terry.trxue@gmail.com>
2016-10-01 02:06:03 -0400

    add rectangle overlapping

--- src/com/terryx/leecode/Solution237.java
+++ src/com/terryx/leecode/Solution237.java
@@ -1,12 +1,42 @@
 package com.terryx.leecode;
 
+import com.terryx.gwu.algorithm.Point2D;
 import com.terryx.main.ListNode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * @author taoranxue on 9/30/16 9:16 PM.
  */
-public class Solution237 {
-    public void deleteNode(ListNode node) {
 
+
+public class Solution237 {
+    public class Line implements Comparable<Line> {
+        Point2D p1;
+        Point2D p2;
+        public Line(Point2D p1, Point2D p2) {
+            this.p1 = p1;
+            this.p2 = p2;
+        }
+
+        @Override
+        public int compareTo(Line other) {
+            return (int)(this.p1.x() - other.p1.x());
+        }
+    }
+
+
+    public void fun() {
+        List<Line> lineList = new ArrayList<>();
+        Collections.sort(lineList);
+    }
+
+    public void deleteNode(ListNode node) {
+        if (node.next == null)
+            return;
+        node.val = node.next.val;
+        node.next = node.next.next;
     }
 }
