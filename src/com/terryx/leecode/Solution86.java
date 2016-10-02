d6e564e20725802aa3c246d126cf5b6bd06871b6
lele94218 <terry.trxue@gmail.com>
2016-10-02 16:15:47 -0400

    finish

--- src/com/terryx/leecode/Solution86.java
+++ src/com/terryx/leecode/Solution86.java
@@ -7,6 +7,22 @@ import com.terryx.main.ListNode;
  */
 public class Solution86 {
     public ListNode partition(ListNode head, int x) {
-        return null;
+        ListNode smallHead = new ListNode(0);
+        ListNode bigHead = new ListNode(0);
+        ListNode cur1 = smallHead;
+        ListNode cur2 = bigHead;
+        while (head != null) {
+            if (head.val < x) {
+                cur1.next = head;
+                cur1 = head;
+            } else {
+                cur2.next = head;
+                cur2 = head;
+            }
+            head = head.next;
+        }
+        cur2.next = null;
+        cur1.next = bigHead.next;
+        return smallHead.next;
     }
 }
--- src/com/terryx/main/Main.java
+++ src/com/terryx/main/Main.java
@@ -9,17 +9,22 @@ public class Main {
     public static void main(String args[]) {
         Solution61 solution61 = new Solution61();
         ListNode head = new ListNode(1);
-//        head.next = new ListNode(4);
-//        head.next.next = new ListNode(3);
+        head.next = new ListNode(4);
+        head.next.next = new ListNode(3);
 
-        ListNode head1 = new ListNode(5);
+//        ListNode head1 = new ListNode(5);
 //        head1.next = new ListNode(6);
 //        head1.next.next = new ListNode(4);
-        head = solution61.rotateRight(null, 1);
-        while (head != null) {
-            System.out.println(head);
-            head = head.next;
-        }
+
+        ListNode cur = head;
+        System.out.println(cur);
+        head = head.next;
+        System.out.println(cur);
+        System.out.println(head);
+//        while (head != null) {
+//            System.out.println(head);
+//            head = head.next;
+//        }
 
     }
 
