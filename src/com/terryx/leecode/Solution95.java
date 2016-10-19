5ab174e0c3296220cc698f32ae4b7ef026245d17
lele94218 <terry.trxue@gmail.com>
2016-10-19 17:14:10 -0400

    add one solution

---
+++ src/com/terryx/leecode/Solution95.java
@@ -0,0 +1,14 @@
+package com.terryx.leecode;
+
+import com.terryx.main.TreeNode;
+
+import java.util.List;
+
+/**
+ * @author taoranxue on 10/18/16 3:28 AM.
+ */
+public class Solution95 {
+    public List<TreeNode> generateTrees(int n) {
+        return null;
+    }
+}
