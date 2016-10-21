e0e622f0cfe39d39b8475a5b2466cf0d3ddaf505
lele94218 <terry.trxue@gmail.com>
2016-10-20 20:21:28 -0400

    add 4 solutions

--- src/com/terryx/leecode/Solution95.java
+++ src/com/terryx/leecode/Solution95.java
@@ -2,6 +2,7 @@ package com.terryx.leecode;
 
 import com.terryx.main.TreeNode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,7 +10,31 @@ import java.util.List;
  */
 public class Solution95 {
     public List<TreeNode> generateTrees(int n) {
-        return null;
+        List<TreeNode> result[] = new List[n + 1];
+        result[0] = new ArrayList<>();
+        if (n == 0) return result[n];
+        result[0].add(null);
+        for (int len = 1; len <= n; ++ len) {
+            result[len] = new ArrayList<>();
+            for (int head = 1; head <= len; ++ head) {
+                for (TreeNode lChild : result[head - 1]) {
+                    for (TreeNode rChild : result[len - head]) {
+                        TreeNode root = new TreeNode(head);
+                        root.left = lChild;
+                        root.right = treeOffset(rChild, head);
+                        result[len].add(root);
+                    }
+                }
+            }
+        }
+        return result[n];
+    }
 
+    private TreeNode treeOffset(TreeNode root, int offset) {
+        if (root == null || offset == 0) return root;
+        TreeNode _root = new TreeNode(root.val + offset);
+        _root.left = treeOffset(root.left, offset);
+        _root.right = treeOffset(root.right, offset);
+        return _root;
     }
 }
