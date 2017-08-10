fef4402877674180fcfa063f0767fe43eb2ee735
lele94218 <terry.trxue@gmail.com>
2017-08-10 21:56:36 +0800

    add 2 solutions

---
+++ src/com/terryx/leecode/Solution565.java
@@ -0,0 +1,27 @@
+package com.terryx.leecode;
+
+/**
+ * @author taoranxue on 8/10/17 9:55 PM.
+ */
+public class Solution565 {
+    public int arrayNesting(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        int res = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            if (nums[i] >= 0) {
+                int cur = nums[i], cnt = 0;
+                while (nums[cur] >= 0) {
+                    int tmp = nums[cur];
+                    if (nums[cur] != 0)
+                        nums[cur] = -nums[cur];
+                    else
+                        nums[cur] = Integer.MIN_VALUE;
+                    cur = tmp;
+                    cnt++;
+                }
+                res = Math.max(res, cnt);
+            }
+        }
+        return res;
+    }
+}
