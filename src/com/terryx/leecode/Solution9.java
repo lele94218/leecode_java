bf98d4f152470f8f795c803adc45108c57b0274a
lele94218 <terry.trxue@gmail.com>
2017-08-31 18:14:36 -0400

    add 2 solutions

---
+++ src/com/terryx/leecode/Solution9.java
@@ -0,0 +1,21 @@
+package com.terryx.leecode;
+
+import java.util.*;
+
+/**
+ * @author taoranxue on 8/31/17 4:44 PM.
+ */
+public class Solution9 {
+    public boolean isPalindrome(int x) {
+        if (x % 10 == 0 && x != 0) return false;
+
+        if (x < 0) return false;
+        int num = 0;
+        while (x > num) {
+            num = num * 10 + x % 10;
+            x /= 10;
+        }
+
+        return num == x || x == (num / 10);
+    }
+}
