b63d687f8987edce7e526673299d3e58f7c35929
lele94218 <terry.trxue@gmail.com>
2018-01-29 21:07:49 -0500

    add contest 59

---
+++ src/com/terryx/leecode/Solution315.java
@@ -0,0 +1,95 @@
+package com.terryx.leecode;
+
+import java.util.*;
+
+/**
+ * @author taoranxue on 1/29/18 4:43 PM.
+ */
+public class Solution315 {
+    // BST
+    static class BstNode {
+        int value, smaller;
+        BstNode left, right;
+
+        BstNode(int value, int smaller) {
+            this.value = value;
+            this.smaller = smaller;
+        }
+    }
+
+    private BstNode insert(BstNode root, int value, int index, int res[], int smaller) {
+        if (root == null) {
+            res[index] = smaller;
+            return new BstNode(value, 0);
+        }
+        if (root.value > value) {
+            root.smaller++;
+            root.left = insert(root.left, value, index, res, smaller);
+        } else {
+            root.right = insert(root.right, value, index, res, root.smaller + smaller + ((root.value < value) ? 1 : 0));
+        }
+        return root;
+    }
+
+    public List<Integer> countSmaller0(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        if (nums == null || nums.length == 0) {
+            return res;
+        }
+        BstNode root = null;
+        int helper[] = new int[nums.length];
+        for (int i = nums.length - 1; i >= 0; --i) {
+            root = insert(root, nums[i], i, helper, 0);
+        }
+        for (int i = 0; i < nums.length; ++i) {
+            res.add(helper[i]);
+        }
+        return res;
+    }
+
+    // Merge sort
+    public List<Integer> countSmaller(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        if (nums == null || nums.length == 0) {
+            return res;
+        }
+        int index[] = new int[nums.length], cnt[] = new int[nums.length];
+        for (int i = 0; i < nums.length; ++i) {
+            index[i] = i;
+        }
+        mergeSort(nums, index, cnt, new int[nums.length], 0, nums.length - 1);
+        for (int i = 0; i < nums.length; ++i) {
+            res.add(cnt[i]);
+        }
+        return res;
+    }
+
+    private void mergeSort(int[] nums, int[] index, int[] cnt, int[] helper, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+        int mid = (start + end) / 2;
+        mergeSort(nums, index, cnt, helper, start, mid);
+        mergeSort(nums, index, cnt, helper, mid + 1, end);
+        merge(nums, index, cnt, helper, start, mid, end);
+    }
+
+    private void merge(int[] nums, int[] index, int[] cnt, int[] helper, int start, int mid, int end) {
+        for (int i = start; i <= end; ++i) {
+            helper[i] = index[i];
+        }
+        int i = start, j = mid + 1, idx = start;
+        while (i <= mid && j <= end) {
+            if (nums[helper[i]] <= nums[helper[j]]) {
+                cnt[helper[i]] += (j - (mid + 1));
+                index[idx++] = helper[i++];
+            } else {
+                index[idx++] = helper[j++];
+            }
+        }
+        while (i <= mid) {
+            cnt[helper[i]] += (j - (mid + 1));
+            index[idx++] = helper[i++];
+        }
+    }
+}
---
+++ src/com/terryx/leecode/contest59/Solution730.java
@@ -0,0 +1,53 @@
+package com.terryx.leecode.contest59;
+
+import java.util.*;
+
+/**
+ * @author taoranxue on 1/29/18 6:04 PM.
+ */
+public class Solution730 {
+    public int countPalindromicSubsequences(String S) {
+        if (S == null || S.length() == 0) {
+            return 0;
+        }
+        int len = S.length();
+        int dp[][][] = new int[len][len][4];
+        for (int i = len - 1; i >= 0; --i) {
+            for (int j = i; j < len; ++j) {
+                for (int k = 0; k < 4; ++k) {
+                    char c = (char) ('a' + k);
+                    if (i == j) {
+                        dp[i][j][k] = (S.charAt(i) == c) ? 1 : 0;
+                    } else {
+                        if (S.charAt(i) != c) {
+                            dp[i][j][k] = dp[i + 1][j][k];
+                        } else if (S.charAt(j) != c) {
+                            dp[i][j][k] = dp[i][j - 1][k];
+                        } else {
+                            if (j == i + 1) {
+                                dp[i][j][k] = 2;
+                            } else {
+                                dp[i][j][k] = 2;
+                                for (int m = 0; m < 4; ++m) {
+                                    dp[i][j][k] = (int) (((long) dp[i][j][k] + dp[i + 1][j - 1][m]) % 1000000007);
+                                }
+                            }
+                        }
+                    }
+                    //System.out.println(i + " " + j + " " + c + " = " + dp[i][j][k]);
+                }
+            }
+        }
+        long res = 0;
+        for (int i = 0; i < 4; ++i) {
+            res += dp[0][len - 1][i];
+            res %= 1000000007;
+        }
+        return (int) res;
+    }
+
+    public static void main(String[] args) {
+        Solution730 m = new Solution730();
+        System.out.println(m.countPalindromicSubsequences("bccb"));
+    }
+}
