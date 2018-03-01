e44f065338bb41ae1fc93dcd498a8ee329a4c1d7
lele94218 <terry.trxue@gmail.com>
2018-03-01 16:32:34 -0500

    update one solution

--- src/com/terryx/leecode/weeklycontest/contest49/Solution673.java
+++ src/com/terryx/leecode/weeklycontest/contest49/Solution673.java
@@ -8,29 +8,27 @@ import java.util.*;
 public class Solution673 {
     public int findNumberOfLIS(int[] nums) {
         int n = nums.length;
-        int dp1[] = new int[n], res = 0;
+        int dp1[] = new int[n], dp2[] = new int[n], res = 0;
+        Arrays.fill(dp2, 1);
         for (int i = 0; i < n; ++i) {
             dp1[i] = 1;
             for (int j = 0; j < i; ++j) {
                 if (nums[i] > nums[j]) {
-                    dp1[i] = Math.max(dp1[i], dp1[j] + 1);
+                    if (dp1[j] + 1 > dp1[i]) {
+                        dp2[i] = dp2[j];
+                        dp1[i] = dp1[j] + 1;
+                    } else if (dp1[j] + 1 == dp1[i]) {
+                        dp2[i] += dp2[j];
+                    }
                 }
             }
             res = Math.max(dp1[i], res);
         }
-        int dp[][] = new int[n][res + 1];
-        for (int k = 1; k <= res; ++k) {
-            for (int i = 0; i < n; ++i) {
-                if (k == 1) dp[i][k] = 1;
-                for (int j = 0; j < i; ++j) {
-                    if (nums[i] > nums[j]) {
-                        dp[i][k] += dp[j][k - 1];
-                    }
-                }
-            }
-        }
         int res0 = 0;
-        for (int i = 0; i < n; ++ i) res0 += dp[i][res];
+        for (int i = 0; i < n; ++i)
+            if (dp1[i] == res) {
+                res0 += dp2[i];
+            }
         return res0;
     }
 
